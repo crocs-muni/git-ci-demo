c344097c93ab7cb734b15edb13d20704933e61b5
Martin Ukrop <mukrop@mail.muni.cz>
2014-10-15 17:51:53 +0200

    new number in main

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 
 int main() {
 	std::cout << "Hello OpenLab!" << std::endl;
-	std::cout << "fact(5) = " << factorial(5) << std::endl;
+	std::cout << "fact(6) = " << factorial(6) << std::endl;
 	return 0;
 }
